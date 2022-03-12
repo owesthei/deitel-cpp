3578b74121108b0ca6ce55134ad7062c67629010
owesthei <owesthei@msn.com>
2022-03-11 21:58:47 -0600

    fig02_05.cpp

--- fig02_05.cpp
+++ fig02_05.cpp
@@ -11,7 +11,7 @@ int main() {
     cout << "Enter two integers to compare: ";
     cin >> number1 >> number2;
 
-    if(number1 == number2) {
+    if (number1 == number2) {
         cout << number1 << " == " << number2 << endl;
     }
 
